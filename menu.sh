5f1aa155d7ec4950044b8008b46bf7ec1a28f392
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-27 19:52:11 +0530

    menu.sh

--- menu.sh
+++ menu.sh
@@ -1,23 +1,12 @@
 while true; do
-    echo -e "\nMenu:"
-    echo "1. List users currently working"
-    echo "2. Copy a file"
-    echo "3. Rename a file"
-    echo "4. List files in a directory"
-    echo "5. Quit"
-    read -p "Enter your choice: " choice
-
-    case $choice in
-        1) who ;;
-        2) read -p "Enter source file: " src
-           read -p "Enter destination directory: " dest
-           cp "$src" "$dest" && echo "File copied successfully." ;;
-        3) read -p "Enter current file name: " old
-           read -p "Enter new file name: " new
-           mv "$old" "$new" && echo "File renamed successfully." ;;
-        4) read -p "Enter directory path: " dir
-           ls "$dir" ;;
-        5) echo "Goodbye!"; exit ;;
-        *) echo "Invalid choice. Please try again." ;;
-    esac
+  echo -e "\n1.List users  2.Copy file  3.Rename  4.List dir  5.Quit"
+  read -p "Enter choice: " c
+  case $c in
+    1) who ;;
+    2) read -p "Src: " s; read -p "Dest: " d; cp "$s" "$d" ;;
+    3) read -p "Old: " o; read -p "New: " n; mv "$o" "$n" ;;
+    4) read -p "Dir: " di; ls "$di" ;;
+    5) exit ;;
+    *) echo "Invalid!" ;;
+  esac
 done
