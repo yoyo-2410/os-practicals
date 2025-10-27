f1ff08e5f6236bb510cb29c1fb41b7a19616e943
𝕐𝕆𝕐𝕆乛 <138222781+yoyo-2410@users.noreply.github.com>
2025-10-27 19:25:04 +0530

    prime_number.sh

--- prime_number.sh
+++ prime_number.sh
@@ -1,10 +1,4 @@
 echo "Prime number checking"
-read -p "Enter a number: " n
-if (( n < 2 )); then
-    echo "$n is not a prime number."
-    exit
-fi
-for ((i=2; i<=n/2; i++)); do
-    ((n%i==0)) && { echo "$n is not a prime number."; exit; }
-done
-echo "$n is a prime number"
+read -p "Enter number: " n
+for((i=2;i<=n/2;i++)); do ((n%i==0)) && { echo "$n is not prime"; exit; }; done
+((n<2)) && echo "$n is not prime" || echo "$n is prime"
